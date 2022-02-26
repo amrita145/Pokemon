c14eda9943bfb4e717cfc7042eef158a716a0a53
amrita <amritarajhibm111@gmail.com>
2022-02-26 16:41:52 +0530

    Updates

---
+++ precache-manifest.2e6dfda77f53c8a582de73098cfdda5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50479f49a6929b1b4157f483985977f",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "60ce4a00b6ecc6b4e874",
+    "url": "/Pokemon/static/css/main.47f666f0.chunk.css"
+  },
+  {
+    "revision": "87c3aeaa0f6960155e16",
+    "url": "/Pokemon/static/js/2.d09f8bdb.chunk.js"
+  },
+  {
+    "revision": "60ce4a00b6ecc6b4e874",
+    "url": "/Pokemon/static/js/main.89c0af84.chunk.js"
+  },
+  {
+    "revision": "3bc49a824e9868aa53bb",
+    "url": "/Pokemon/static/js/runtime~main.c2748737.js"
+  }
+]);
